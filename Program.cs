bd41f20bfe2da6d8c8ce1b8ece11823bcd6ccea7
tgochitashvili <tazo.gochitashvili@gmail.com>
2023-06-02 16:44:10 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TBCAcademyCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+        }
+    }
+}
