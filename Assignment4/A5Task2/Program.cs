23d7b21e351c2648108f96f10b5b73f454823029
tgochitashvili <tazo.gochitashvili@gmail.com>
2023-06-12 16:56:19 +0400

    Assignment5

---
+++ Assignment4/A5Task2/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace Assignment5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (; ; )
+            {
+                char[] arr = new char[5]; // ლისტი ჯობია
+                int currItem = 0;
+                for (; ; )
+                {
+                    try
+                    {
+                        Console.WriteLine("Add a char to the list (q! to stop)");
+                        string inp = Console.ReadLine();
+
+                        if (inp.ToLower() == "q!")
+                            break;
+                        else
+                        {
+                            if (currItem >= arr.Length)
+                                ResizeArray(ref arr);
+                            arr[currItem] = Convert.ToChar(inp);
+                            currItem++;
+                        }
+                    }
+                    catch (Exception x)
+                    {
+                        Console.WriteLine("Enter a valid char or 'q!' to stop");
+                    }
+                }
+
+                for (; ; )
+                {
+                    try
+                    {
+                        Console.WriteLine("Enter a char to search for");
+                        char element = Convert.ToChar(Console.ReadLine());
+                        int count = CountElements(arr, element);
+
+                        Console.WriteLine($"The element {element} appears {count} times");
+                    }
+                    catch(Exception x)
+                    {
+                        Console.WriteLine("Enter a valid char");
+                    }
+                }
+            }
+        }
+
+        static void ResizeArray(ref char[] arr, double rate = 1.5)
+        {
+            char[] tempArr = new char[Convert.ToInt32(arr.Length * rate)];
+            arr.CopyTo(tempArr, 0);
+            arr = tempArr;
+        }
+        static int CountElements(char[] arr, char element)
+        {
+            int count = default;
+            foreach (char item in arr)
+            {
+                if (item == element)
+                    count++;
+            }
+            return count;
+        }
+    }
+}
---
+++ Assignment5/A5Task1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Text;
+
+namespace Assignment5
+{
+    public class A5Task1
+    {
+        static void Main(string[] args)
+        {
+            for(; ; )
+            {
+                int[] arr = new int[5]; // ლისტი ჯობია
+                int currItem = 0;
+                for(; ; )
+                {
+                    try
+                    {
+                        Console.WriteLine("Add an integer to the list (q to stop)");
+                        string inp = Console.ReadLine();
+
+                        if (inp.ToLower() == "q")
+                            break;
+                        else
+                        {
+                            if (currItem >= arr.Length)
+                                ResizeArray(ref arr);
+                            arr[currItem] = Convert.ToInt32(inp);
+                            currItem++;
+                        }
+                    }
+                    catch (Exception x) 
+                    {
+                        Console.WriteLine(x.Message);
+                        Console.WriteLine(x.StackTrace);
+                    }
+                }
+                Console.WriteLine("Array: " + FormatThing(arr));
+                for(; ; )
+                {
+                    try
+                    {
+                        Console.WriteLine("Enter an index to sum an element's digits (q to stop)");
+                        string inp = Console.ReadLine();
+                        if (inp.ToLower() == "q")
+                            break;
+                        else
+                        {
+                            int element = arr[Convert.ToInt32(inp)];
+                            Console.WriteLine($"Sum of {element}'s digits: {SumOfDigits(element)}");
+                        }
+
+                    }
+                    catch { }
+                }
+            }
+
+        }
+        public static string FormatThing(dynamic arr)
+        {
+            if (arr.Length == 0)
+                return "";
+            StringBuilder outputBuilder = new StringBuilder();
+            outputBuilder.Insert(0, "[");
+            foreach (dynamic d in arr)
+            {
+                outputBuilder.Append(d + ", ");
+            }
+
+            outputBuilder.Remove(outputBuilder.Length - 2, 2);
+            outputBuilder.Append("]");
+
+            return outputBuilder.ToString();
+        }
+        public static void ResizeArray(ref int[] arr, double rate = 1.5)
+        {
+            int[] tempArr = new int[Convert.ToInt32(arr.Length * rate)];
+            arr.CopyTo(tempArr, 0);
+            arr = tempArr;
+        }
+        public static int SumOfDigits(int n)
+        {
+            int sum = 0;
+            for(int i = 0; i < n.ToString().Length; i++)
+            {
+                sum += (n / (int)Math.Pow(10, i)) % 10;
+            }
+            return sum;
+        }
+    }
+}
